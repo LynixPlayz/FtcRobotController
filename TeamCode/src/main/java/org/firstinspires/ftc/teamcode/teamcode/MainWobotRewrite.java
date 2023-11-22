2c841cdaf0c938e47b216e2f57319dafb164948c
LynixPlayz <82721356+LynixPlayz@users.noreply.github.com>
2023-11-21 18:20:25 -0600

    arm stuff1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/MainWobotRewrite.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/MainWobotRewrite.java
@@ -2,77 +2,70 @@ package org.firstinspires.ftc.teamcode.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="MianWobotRewrite", group="Iterative Opmode")
 public class MainWobotRewrite extends LinearOpMode {
 
-    public DcMotor frontLeft = null;
-    public DcMotor backLeft = null;
-    public DcMotor frontRight = null;
-    public DcMotor backRight = null;
-    public DcMotor armLeft = null;
-    public DcMotor armRight = null;
+    public DcMotorEx frontLeft = null;
+    public DcMotorEx backLeft = null;
+    public DcMotorEx frontRight = null;
+    public DcMotorEx backRight = null;
+    //public DcMotorEx armLeft = null;
+    public DcMotorEx arm = null;
 
     private Servo gripper = null;
     private Servo wrist = null;
 
     private boolean manualMode = false;
-    private double armSetpoint = 0.0;
 
-    private final double armManualDeadband = 0.03;
-
-    private final double gripperClosedPosition = 1.0;
-    private final double gripperOpenPosition = 0;
     private final double wristUpPosition = 1.0;
     private final double wristDownPosition = -1.0;
 
     private final int armHomePosition = 20;
-    private final int armIntakePosition = 30;
-    private final int armScorePosition = -600;
-    private final int armShutdownThreshold = 5;
-
-    private boolean debug = true;
-    private boolean debugPositions = true;
 
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
 
-        frontLeft = hardwareMap.get(DcMotor.class, "leftDriveFront");
-        frontRight = hardwareMap.get(DcMotor.class, "rightDriveFront");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "leftDriveFront");
+        frontRight = hardwareMap.get(DcMotorEx.class, "rightDriveFront");
 
-        backLeft = hardwareMap.get(DcMotor.class, "leftDriveBack");
-        backRight = hardwareMap.get(DcMotor.class, "rightDriveBack");
+        backLeft = hardwareMap.get(DcMotorEx.class, "leftDriveBack");
+        backRight = hardwareMap.get(DcMotorEx.class, "rightDriveBack");
 
 
-        armLeft  = hardwareMap.get(DcMotor.class, "armLeft");
-        armRight = hardwareMap.get(DcMotor.class, "armRight");
+        ////armLeft  = hardwareMap.get(DcMotorEx.class, "armLeft");
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
         gripper = hardwareMap.get(Servo.class, "gripper");
         wrist = hardwareMap.get(Servo.class, "wrist");
 
-        armLeft.setDirection(DcMotor.Direction.FORWARD);
-        armRight.setDirection(DcMotor.Direction.REVERSE);
-        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armLeft.setPower(0.0);
-        armRight.setPower(0.0);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        ////armLeft.setDirection(DcMotorEx.Direction.FORWARD);
+        arm.setDirection(DcMotorEx.Direction.REVERSE);
+        ////armLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        ////armLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        arm.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        ////armLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        ////armLeft.setPower(0.0);
+        arm.setPower(0.0);
 
         waitForStart();
 
-        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armLeft.setTargetPosition(armHomePosition);
-        armRight.setTargetPosition(armHomePosition);
-        armLeft.setPower(1.0);
-        armRight.setPower(1.0);
-        armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //armLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        //armLeft.setTargetPosition(armHomePosition);
+        arm.setTargetPosition(armHomePosition);
+        //armLeft.setVelocity(1000);
+        arm.setVelocity(1000);
+        //armLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
         while(opModeIsActive())
         {
@@ -91,10 +84,8 @@ public class MainWobotRewrite extends LinearOpMode {
 
     public void telemetryUpdate()
     {
-        if (debug) {
-            telemetry.addData("Gamepad Inputs", getGamepadDebugInfo() + "\n\n" + getMotorsDebugInfo());
-            telemetry.update();
-        }
+        telemetry.addData("Gamepad Inputs", getGamepadDebugInfo() + "\n\n" + getMotorsDebugInfo());
+        telemetry.update();
     }
 
     public String getGamepadDebugInfo()
@@ -128,16 +119,16 @@ public class MainWobotRewrite extends LinearOpMode {
     {
         String returnString = "";
 
-        returnString += "leftArmMotor " + armLeft.getCurrentPosition() + "\n";
-        returnString += "rightArmMotor " + armRight.getCurrentPosition() + "\n";
+        //returnString += "leftArmMotor " + armLeft.getCurrentPosition() + "\n";
+        returnString += "rightArmMotor " + arm.getCurrentPosition() + "\n";
 
         returnString += "leftDriveFront " + frontLeft.getCurrentPosition() + "\n";
         returnString += "rightDriveBack " + backLeft.getCurrentPosition() + "\n";
         returnString += "leftDriveFront " + frontRight.getCurrentPosition() + "\n";
         returnString += "rightDriveBack " + backRight.getCurrentPosition() + "\n";
         returnString += "leftDriveFront " + frontRight.getCurrentPosition() + "\n";
-        returnString += "leftArmTarget " + armLeft.getTargetPosition() + "\n";
-        returnString += "rightArmTarget " + armRight.getTargetPosition() + "\n";
+        //returnString += "leftArmTarget " + armLeft.getTargetPosition() + "\n";
+        returnString += "rightArmTarget " + arm.getTargetPosition() + "\n";
         returnString += "wrist " + wrist.getPosition() + "";
 
 
@@ -146,39 +137,28 @@ public class MainWobotRewrite extends LinearOpMode {
 
     public void processJoystickInputToMovement()
     {
-        double frontLeftCalibration = 1.14;
-        double backLeftCalibration = 1.21;
-        double frontRightCalibration = 1.09;
-        double backRightCalibration = 1.15;
-        /*double y = -gamepad2.left_stick_y;
-        double x = gamepad2.left_stick_x;
+        double y = -gamepad1.left_stick_x; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_y * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
 
-        double turn = gamepad2.right_stick_x;
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
 
-        //math stuff I dont understand :P
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(turn), 1);
+        double frontLeftCalib = 1;
+        double backLeftCalib = 1;
+        double frontRightCalib = 1;
+        double backRightCalib = 1;
 
-        frontLeft.setPower( ((y + x + turn) / denominator) / 2.6 );
-        backLeft.setPower( ((y - x + turn) / denominator ) / 1.626);
-        frontRight.setPower( ((y - x - turn) / denominator) / 2.52);
-        backRight.setPower( (( y + x - turn) / denominator) / 2.594);*/
-
-        // Strafing on Right Stick using the X-axis
-        // Move the stick right and left for the robot to slide right and left
-        frontLeft.setPower(gamepad1.right_stick_x);
-        backLeft.setPower(-gamepad1.right_stick_x);
-        frontRight.setPower(gamepad1.right_stick_x);
-        backRight.setPower(-gamepad1.right_stick_x);
-        // Turning on Left Joystick using X-Axis
-        frontLeft.setPower(gamepad1.left_stick_x);
-        backLeft.setPower(gamepad1.left_stick_x);
-        frontRight.setPower(gamepad1.left_stick_x);
-        backRight.setPower(gamepad1.left_stick_x);
-        // Forwards and Back on Left Stick using the Y-axis
-        frontLeft.setPower(-gamepad1.left_stick_y);
-        backLeft.setPower(-gamepad1.left_stick_y);
-        frontRight.setPower(gamepad1.left_stick_y);
-        backRight.setPower(gamepad1.left_stick_y);
+        frontLeft.setPower(frontLeftPower / frontLeftCalib);
+        backLeft.setPower(backLeftPower / backLeftCalib);
+        frontRight.setPower(frontRightPower / frontRightCalib);
+        backRight.setPower(backRightPower / backRightCalib);
 
     }
 
@@ -186,12 +166,12 @@ public class MainWobotRewrite extends LinearOpMode {
     {
         if(gamepad1.x)
         {
-            armLeft.setTargetPosition(armHomePosition);
-            armRight.setTargetPosition(armHomePosition);
-            armLeft.setPower(1.0);
-            armRight.setPower(1.0);
-            armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //armLeft.setTargetPosition(armHomePosition);
+            arm.setTargetPosition(armHomePosition);
+            //armLeft.setVelocity(1000);
+            arm.setVelocity(1000);
+            //armLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
             wrist.setPosition(wristUpPosition);
         }
     }
@@ -200,16 +180,17 @@ public class MainWobotRewrite extends LinearOpMode {
     {
         double manualArmPower;
         manualArmPower = gamepad1.right_trigger - gamepad1.left_trigger;
+        double armManualDeadband = 0.03;
         if (Math.abs(manualArmPower) > armManualDeadband) {
             if (!manualMode) {
-                armLeft.setPower(0.0);
-                armRight.setPower(0.0);
-                armLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                armRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                //armLeft.setPower(0.0);
+                arm.setPower(0.0);
+                //armLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+                arm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
                 manualMode = true;
             }
-            armLeft.setPower(manualArmPower);
-            armRight.setPower(manualArmPower);
+            //armLeft.setPower(manualArmPower);
+            arm.setPower(manualArmPower);
         }
     }
 
@@ -217,22 +198,23 @@ public class MainWobotRewrite extends LinearOpMode {
     {
         if(gamepad1.a)
         {
-            armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            armLeft.setPower(0);
-            armRight.setPower(0);
+            //armLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            arm.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            //armLeft.setPower(0);
+            arm.setPower(0);
         }
     }
 
     public void processGamepadScoringPos()
     {
         if (gamepad1.y) {
-            armLeft.setTargetPosition(armScorePosition);
-            armRight.setTargetPosition(armScorePosition);
-            armLeft.setPower(0.5);
-            armRight.setPower(0.5);
-            armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            int armScorePosition = -600;
+            //armLeft.setTargetPosition(armScorePosition);
+            arm.setTargetPosition(armScorePosition);
+            //armLeft.setVelocity(600);
+            arm.setVelocity(600);
+            //armLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
             wrist.setPosition(wristDownPosition);
         }
     }
@@ -250,9 +232,11 @@ public class MainWobotRewrite extends LinearOpMode {
     public void processGamepadGripperMovement()
     {
         if (gamepad1.left_bumper || gamepad1.right_bumper) {
+            double gripperOpenPosition = 0;
             gripper.setPosition(gripperOpenPosition);
         }
         else {
+            double gripperClosedPosition = 1.0;
             gripper.setPosition(gripperClosedPosition);
         }
     }
